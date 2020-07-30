f2a53a8c02abbec5365e498b984b9f29fe09bb10
corralperez <corralperez@gmail.com>
2020-07-30 18:26:06 +0200

    first commit

---
+++ src/app/components/components.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-components',
+    templateUrl: './components.component.html',
+    styles: [`
+    ngb-progressbar {
+        margin-top: 5rem;
+    }
+    `]
+})
+
+export class ComponentsComponent  {
+   
+
+}
