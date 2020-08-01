b8c2227553128b4755627c8bfb6850e0f154f43d
corralperez <corralperez@gmail.com>
2020-08-01 19:51:21 +0200

    Remove unused components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,14 +6,10 @@ import { NouisliderModule } from 'ng2-nouislider';
 import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
 import { RouterModule } from '@angular/router';
 
-import { BasicelementsComponent } from './basicelements/basicelements.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { TypographyComponent } from './typography/typography.component';
 import { NucleoiconsComponent } from './nucleoicons/nucleoicons.component';
 import { ComponentsComponent } from './components.component';
 import { NotificationComponent } from './notification/notification.component';
-import { NgbdModalComponent } from './modal/modal.component';
-import { NgbdModalContent } from './modal/modal.component';
 
 @NgModule({
     imports: [
@@ -26,15 +22,10 @@ import { NgbdModalContent } from './modal/modal.component';
     ],
     declarations: [
         ComponentsComponent,
-        BasicelementsComponent,
         NavigationComponent,
-        TypographyComponent,
         NucleoiconsComponent,
         NotificationComponent,
-        NgbdModalComponent,
-        NgbdModalContent
     ],
-    entryComponents: [NgbdModalContent],
     exports:[ ComponentsComponent ]
 })
 export class ComponentsModule { }
