70512a044fdf0f2219472c6333603b8e8978e684
corralperez <corralperez@gmail.com>
2020-08-14 19:35:57 +0200

    Clean code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [`
-    ngb-progressbar {
-        margin-top: 5rem;
-    }
-    `]
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 }
