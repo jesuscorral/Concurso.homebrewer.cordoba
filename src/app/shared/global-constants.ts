34f9190c57fd53ee7ed21aa20666de2af8c32847
Jesus Corral Perez <jesus@sidra.dev>
2024-06-18 18:43:46 +0200

    Changes for 2024

--- src/app/shared/global-constants.ts
+++ src/app/shared/global-constants.ts
@@ -1,8 +1,8 @@
 export class GlobalConstants {
-    public static editionNumber: string = "VII";
-    public static day: string = "25";
+    public static editionNumber: string = "VIII";
+    public static day: string = "23";
     public static month: string = "Noviembre";
-    public static year: string = "2023";
+    public static year: string = "2024";
     public static startReceptionDate: string = "10 de Noviembre";
     public static endReceptionDate: string = "17 de Noviembre";
 }
