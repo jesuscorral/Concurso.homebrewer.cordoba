80bd273d7a0db39b43e7cc1ecbad27c19b4f71f4
Jesus Corral Perez <jesus@sidra.dev>
2025-06-21 13:40:08 +0200

    Remove angular project

--- src/app/rules/rule.model.ts
+++
@@ -1,9 +0,0 @@
-export class Rule {
-    title: string;
-    content: string;
-    icon: string;
-
-    public constructor(init?: Partial<Rule>) {
-        Object.assign(this, init);
-      }
-}
--- src/app/shared/global-constants.ts
+++
@@ -1,8 +0,0 @@
-export class GlobalConstants {
-    public static editionNumber: string = "VIII";
-    public static day: string = "23";
-    public static month: string = "Noviembre";
-    public static year: string = "2024";
-    public static startReceptionDate: string = "10 de Noviembre";
-    public static endReceptionDate: string = "17 de Noviembre";
-}
